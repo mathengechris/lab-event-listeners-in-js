c4ed77a3b0b727aa2e90922fa617a585b5bc8132
mathengechris <mathengechris27@gmail.com>
2026-01-02 21:44:53 -0800

    initial

--- index.js
+++ index.js
@@ -3,11 +3,19 @@
 // Function to change the background color when a button is clicked
 function changeBackgroundColor() {
   // Implement the function to change background color
+ 
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+    document.body.style.backgroundColor= `rgb(${r}, ${g}, ${b})`;
+
 }
 
 // Function to reset the background color when the body is double-clicked
 function resetBackgroundColor() {
   // Implement the function to reset background color
+  document.body.style.backgroundColor = '';
 }
 
 // Capture Keyboard Input
@@ -15,6 +23,8 @@ function resetBackgroundColor() {
 // Function to display the key pressed by the user
 function displayKeyPress(event) {
   // Implement the function to display key pressed
+  const keyPressDisplay = document.getElementById('keyPressDisplay')
+  keyPressDisplay.textContent = `key pressed : ${event.key}`
 }
 
 // Process Text Input
@@ -22,6 +32,9 @@ function displayKeyPress(event) {
 // Function to display user input in real-time
 function displayUserInput() {
   // Implement the function to display user input
+  const textInput= document.getElementById('textInput')
+  const textInputDisplay= document.getElementById('textInputDisplay')
+  textInputDisplay.textContent = `You typed : ${textInput.value}`
 }
 
 // Attach Event Listeners
@@ -55,3 +68,6 @@ module.exports = {
   displayUserInput,
   setupEventListeners,
 }
+
+
+
